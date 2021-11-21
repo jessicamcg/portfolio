996fad7686e0e8906997c353a6dabc56511f392e
jessicamcg <jessicamcguico@gmail.com>
2021-11-20 20:26:25 -0600

    components folder

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+export default function Footer() {
+  
+    return (
+      <div>
+          
+          {/* <div><p>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></p></div> */}
+      </div>
+    );
+  }
