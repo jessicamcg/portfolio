3990ca307e5cc4a02a1641d620a022c3127efc85
jessicamcg <jessicamcguico@gmail.com>
2021-11-20 21:55:56 -0600

    rendered an image, centered

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,13 +1,15 @@
 import Footer from "./Footer";
 import NavTabs from "./Navbar";
+import SupIcon from "../images/greetings.png"
 
 export default function PortfolioContainer() {
-  
+    const styles = {
+        width: "200px"
+      };
     return (
-      <div>
+      <div className="d-flex flex-column align-middle justify-content-center">
         <NavTabs />
-        <h1>sup</h1>
-        <img src={require("../images/greetings.png")} alt={"sup"}/>
+        <img className="mx-auto my-3" style={styles }src={SupIcon} alt={"sup"}/>
         <Footer/>
       </div>
     );
