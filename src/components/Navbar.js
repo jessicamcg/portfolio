d524f7b9b17130232f40d928c80394c7d8be9ae9
jessicamcg <jessicamcguico@gmail.com>
2021-11-22 00:32:58 -0600

    back to top nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,45 +1,33 @@
 import React from 'react';
 
 function NavTabs(currentPage) {
-    const styles = {
-        position: "fixed",
-        right: "0",
-        bottom: "0",
-        left: "90%",
-        zIndex: "1030"
-      };
+    // const styles = {
+    //     position: "fixed",
+    //     right: "0",
+    //     bottom: "0",
+    //     left: "90%",
+    //     zIndex: "1030"
+    //   };
     return (
         <nav id="navbar">
-            <ul className="nav flex-column" style={styles}>
+            <ul className="nav justify-content-center" >
                 <li className="nav-item">
-                    <a
-                        href="#home"
-                        className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
-                    >
+                    <a href="#home" className="nav-link">
                         home.
                     </a>
                 </li>
                 <li className="nav-item">
-                    <a
-                        href="#about"
-                        className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
-                    >
+                    <a href="#about" className="nav-link">
                         about.
                     </a>
                 </li>
                 <li className="nav-item">
-                    <a
-                        href="#projects"
-                        className={currentPage === 'Blog' ? 'nav-link active' : 'nav-link'}
-                    >
+                    <a href="#projects" className="nav-link">
                         projects.
                     </a>
                 </li>
                 <li className="nav-item">
-                    <a
-                        href="#contact"
-                        className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
-                    >
+                    <a href="#contact" className="nav-link">
                         contact.
                     </a>
                 </li>
