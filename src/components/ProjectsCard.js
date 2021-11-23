10277bc907245017d92a6237b69c813eeec26d46
jessicamcg <jessicamcguico@gmail.com>
2021-11-22 22:41:08 -0600

    mui for projectscontainer, will modularize later

---
+++ src/components/ProjectsCard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function ProjectsCard() {
+    return (
+        <div id="projects">
+            <h4 ></h4>
+            <p>"project 3 placeholder"</p>
+            <p>trip tunes</p>
+            <p>tech blog</p>
+            <p>dnd character tracker</p>
+        </div>
+    )
+    
+}
+
+export default ProjectsCard;
--- src/components/ProjectsContainer.js
+++ src/components/ProjectsContainer.js
@@ -1,16 +1,94 @@
-import React from 'react';
+import * as React from 'react';
+import Accordion from '@mui/material/Accordion';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
-function ProjectsContainer() {
+export default function ControlledAccordions() {
+    const [expanded, setExpanded] = React.useState(false);
+
+    const handleChange = (panel) => (event, isExpanded) => {
+        setExpanded(isExpanded ? panel : false);
+    };
+
     return (
         <div>
-            <h4 id="projects"></h4>
-            <p>"project 3 placeholder"</p>
-            <p>trip tunes</p>
-            <p>tech blog</p>
-            <p>dnd character tracker</p>
+            <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1bh-content"
+                    id="panel1bh-header"
+                >
+                    <Typography sx={{ width: '33%', flexShrink: 0 }}>
+                        IMG
+                    </Typography>
+                    <Typography sx={{ color: 'text.secondary' }}>DND Character Tracker</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>
+                        description
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+            <Accordion expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel2bh-content"
+                    id="panel2bh-header"
+                >
+                    <Typography sx={{ width: '33%', flexShrink: 0 }}>
+                        IMG
+                    </Typography>
+                    <Typography sx={{ color: 'text.secondary' }}>
+                        Trip Tunes
+                    </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>
+                        description
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+            <Accordion expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel3bh-content"
+                    id="panel3bh-header"
+                >
+                    <Typography sx={{ width: '33%', flexShrink: 0 }}>
+                        IMG
+                    </Typography>
+                    <Typography sx={{ color: 'text.secondary' }}>
+                        Tech Blog
+                    </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>
+                        description
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+            <Accordion expanded={expanded === 'panel4'} onChange={handleChange('panel4')}>
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel4bh-content"
+                    id="panel4bh-header"
+                >
+                    <Typography sx={{ width: '33%', flexShrink: 0 }}>
+                        IMG
+                    </Typography>
+                    <Typography sx={{ color: 'text.secondary' }}>
+                        Project 3 Placeholder
+                    </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>
+                        Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit
+                        amet egestas eros, vitae egestas augue. Duis vel est augue.
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
         </div>
-    )
-    
+    );
 }
-
-export default ProjectsContainer;
