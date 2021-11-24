027ab4c56e5f45aae7e627f71ca0f5079b473ce6
jessicamcg <jessicamcguico@gmail.com>
2021-11-23 23:02:40 -0600

    css for home page

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,17 +1,12 @@
 import React, { useState } from "react";
 import Footer from "./Footer";
 import NavBar from "./Navbar";
-import SupIcon from "../images/greetings.png"
 import Body from "./Body";
 
 export default function PortfolioContainer() {
-    const styles = {
-        width: "200px"
-      };
     return (
       <div className="d-flex flex-column align-middle justify-content-center">
         <NavBar />
-        <img className="mx-auto my-5" style={styles} src={SupIcon} alt={"sup"}/>
         <Body/>
         <Footer/>
       </div>
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,11 +1,37 @@
+import { Box } from '@mui/system';
 import React from 'react';
+import ProfilePic from '../../images/profilepic.JPG'
+
 function About() {
+    
+    const styles = {
+        width: "200px",
+        height: "200px",
+        borderRadius: "50%",
+        border: "8px solid #FFCCBC"
+      };
     return (
-        <div>            
-            <h4 id="about">Jessica Guico</h4>
-            <p>stuff about me</p>
-            <p>bio</p>
-        </div>
+        <Box 
+            id="about" 
+            sx={{ display: 'flex', flexDirection: 'row', width:4/5, justifyContent: 'space-evenly', alignContent: 'center', height:'100vh' }} 
+            className="mx-auto my-3"
+        >
+            <img 
+                src = {`${ProfilePic}`}
+                style = {styles}    
+                className="my-auto"
+            />            
+            <Box
+                sx={{width:3/5, flexGrow: 1}}
+                className="my-auto"
+            >
+                <h4>Jessica Guico</h4>
+                <p>Full Stack Web Developer</p>
+                <p>Houston, Texas</p>
+            </Box>
+            
+
+        </Box>
     )
 }
 
