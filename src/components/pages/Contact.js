348dbdbe0929e6be3f66b1a260684d305c3b0c76
jessicamcg <jessicamcguico@gmail.com>
2022-02-28 15:45:50 -0600

    resume link and email added

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import AssignmentIndIcon from '@mui/icons-material/AssignmentInd';
 import { createTheme } from "@mui/material/styles";
 import { ThemeProvider } from "@mui/material";
 
@@ -25,6 +26,9 @@ function Contact() {
             <h4 id="contact">contact me</h4>
             <a href="https://github.com/jessicamcg" target="_blank"><GitHubIcon color="secondary"/></a>
             <a href="https://www.linkedin.com/in/jessica-guico-833862222/" target="_blank"><LinkedInIcon color="secondary"/></a>
+            <a href="https://docs.google.com/document/d/1cLpCixquGUS_2y4p8lZQ49vDviO3sdaHgpwgFUz787s/export?format=pdf"><AssignmentIndIcon color="secondary"/></a>
+            <a href="mailto:jessicamcguico@gmail.com">jessicamcguico@gmail.com</a>
+
         </div>
     </ThemeProvider>
     )
